1e59e5a4c3c273b518d35a3cd8498692f61dab58
R-YaTian <47445484+R-YaTian@users.noreply.github.com>
2023-09-05 16:14:03 +0800

    Update build batch

--- build.bat
+++ build.bat
@@ -5,12 +5,12 @@ set INCLUDE=%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.1A\Include;%INCLUDE%
 set PATH=%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.1A\Bin;%PATH%
 set LIB=%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.1A\Lib;%LIB%
 
-mkdir C:\OpenSSL
-perl Configure VC-WIN32 no-asm no-shared no-dso --prefix=C:/OpenSSL -D_WIN32_WINNT=0x0501 -D_USING_V110_SDK71_ -L/subsystem:console,"5.01"
+mkdir D:\OpenSSL
+perl Configure VC-WIN32 no-asm no-shared no-dso --prefix=D:/OpenSSL -D_WIN32_WINNT=0x0501 -D_USING_V110_SDK71_
 call ms\do_ms
 nmake -f ms\nt.mak
 nmake -f ms\nt.mak install
 
-cd /d C:\
+cd /d D:\
 7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -sse OpenSSL-1.0.2u-Static-XP.7z OpenSSL
 pause
