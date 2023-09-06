65568a86bc59d3b59ac889dc1769761ed5cb3552
R-YaTian <47445484+R-YaTian@users.noreply.github.com>
2023-09-06 16:54:28 +0800

    Update build_shared.bat

--- build_shared.bat
+++ build_shared.bat
@@ -8,8 +8,8 @@ set LIB=%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.1A\Lib;%LIB%
 mkdir C:\OpenSSL
 perl Configure VC-WIN32 no-asm --prefix=C:/OpenSSL -D_WIN32_WINNT=0x0501 -D_USING_V110_SDK71_
 call ms\do_ms
-nmake -f ms\nt.mak
-nmake -f ms\nt.mak install
+nmake -f ms\ntdll.mak
+nmake -f ms\ntdll.mak install
 
 cd /d C:\
 7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -sse OpenSSL-1.0.2u-XP.7z OpenSSL
